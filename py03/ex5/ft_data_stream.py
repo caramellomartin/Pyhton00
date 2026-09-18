b508df7ed041360e3e5fefb3194b9843f017f1e6
Martin <martincaramelloutn@gmail.com>
2026-09-18 14:28:26 +0200

    ex6

--- py03/ex5/ft_data_stream.py
+++ py03/ex5/ft_data_stream.py
@@ -19,9 +19,10 @@ def gen_event() -> Generator[tuple[str, str], None, None]:
 
 def consume_event(events: list[tuple[str, str]]
                   ) -> Generator[tuple[str, str], None, None]:
-    while events:
+    while len(events) > 0:
         index = random.randrange(len(events))
-        yield events.pop(index)
+        chosen_event = events.pop(index)
+        yield chosen_event
 
 
 if __name__ == "__main__":
@@ -29,13 +30,15 @@ if __name__ == "__main__":
 
     event_stream = gen_event()
     for i in range(1000):
-        player, action = next(event_stream)
-        print(f"Event {i}: Player {player} did action {action}")
+        curr_event = next(event_stream)
+        player_name = curr_event[0]
+        player_action = curr_event[1]
+        print(f"Event {i}: Player {player_name} did action {player_action}")
 
-    event_list = []
+    event_list: list[tuple[str, str]] = []
     for _ in range(10):
-        event_l = next(event_stream)
-        event_list.append(event_l)
+        new_event = next(event_stream)
+        event_list.append(new_event)
     print(f"Built list of 10 events: {event_list}")
 
     for event in consume_event(event_list):
